0782dd9eba33f62f7206badf108d51bc267eec28
bhoomikag203 <bhoomikag203@gmail.com>
2018-07-19 15:13:17 +0530

    Finish Static-pages

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "StaticPagesController" , :type => :controller do
+	context "home page" do
+		it "should return 200 status code" do
+			#get :home
+			#expect(response.status).to eq 200
+		end
+	end
+end
