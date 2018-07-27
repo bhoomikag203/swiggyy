0c0b691965d1099c4ddc0876a8119e138d404176
bhoomikag203 <bhoomikag203@gmail.com>
2018-07-27 08:16:28 +0530

    UI implimentation to signup and login forms

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -1,16 +1,18 @@
 class Users::OmniauthController < ApplicationController
+
 	def google_oauth2 
 		@user = User.create_from_google_data(request.env['omniauth.auth'])
 		if @user.persisted?
 			sign_in_and_redirect @user, event: :authentication
 			set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
 		else
-			redirect_to new_user_registration_url, :flash => {:error => 'There was a problem signing you in through Google. Please register or try signing in later.' }
+			redirect_to restaurants_path, :flash => {:error => 'There was a problem signing you in through Google. Please register or try signing in later.' }
 		end
 	end
 
 	def failure
-		puts env['omniauth.error'].inspect
+		puts ENV['omniauth.error'].inspect
 	  	redirect_to new_user_registration_url, :flash => {:error => 'There was a problem signing you in. Please register or try signing in later.' }
 	end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,5 @@ class Restaurant < ApplicationRecord
 	validates :phno, presence: true, length: { maximum: 10 }
 	validates :category, presence: true
 	validates :rating, presence: true
+	has_and_belongs_to_many :users
 end
