293149d99d4c5c9e738d8ff8ade74ac204d1069b
bhoomikag203 <bhoomikag203@gmail.com>
2018-07-30 12:55:50 +0530

    Adding default role to user

--- db/migrate/20180730072053_add_defaul_to_user_role.rb
+++ db/migrate/20180730072053_add_defaul_to_user_role.rb
@@ -1,6 +1,5 @@
 class AddDefaulToUserRole < ActiveRecord::Migration[5.1]
   def change
-    add_column :user_roles, :role, :integer
-    add_column :user_roles, :default, :0
+    change_column :users, :role, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730071718) do
+ActiveRecord::Schema.define(version: 20180730072053) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 20180730071718) do
     t.boolean "admin", default: false
     t.string "image"
     t.integer "restaurant_id"
-    t.integer "role"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
