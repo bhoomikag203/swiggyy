45ab41b9fe06d16df9be7d91bbc316622bfc78e7
bhoomikag203 <bhoomikag203@gmail.com>
2018-07-30 12:04:14 +0530

    Adding migration to User and Restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180728162502) do
+ActiveRecord::Schema.define(version: 20180730062927) do
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "catagories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "image"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -20,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180728162502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rating"
+    t.integer "user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -48,6 +80,7 @@ ActiveRecord::Schema.define(version: 20180728162502) do
     t.string "uid", limit: 500, default: "", null: false
     t.boolean "admin", default: false
     t.string "image"
+    t.integer "restaurant_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
